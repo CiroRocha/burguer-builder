802fe6187a9abc423ab5ff2d07ce699a9d30bd03
Ciro Rocha <ciro.rocha@tegrus.com.br>
2020-08-26 17:27:56 -0300

    Redux DevTools

--- src/store/ReduxWrapper.js
+++ src/store/ReduxWrapper.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, compose } from 'react-redux';
 import { createStore as reduxCreateStore } from 'redux';
 import burgerReducer from './reducers/reducer';
 
-const createStore = () => reduxCreateStore(burgerReducer);
+const createStore = () => reduxCreateStore(
+  burgerReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose
+);
 
 export default ({ element }) => (
   <Provider store={createStore()}>{element}</Provider>
